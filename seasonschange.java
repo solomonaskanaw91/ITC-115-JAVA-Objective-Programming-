2d03a1257b54034b45d79e1bb62f26709cc6fd13
Solomon <solomon.ab1988@gmail.com>
2018-10-19 18:25:55 -0700

    Homework 4 Chapter 4 (Exercise #3)
    4th Ed of textbook, p.309, ex. 3 Write a method called season that takes as parameters two integers….

---
+++ seasonschange.java
@@ -0,0 +1,28 @@
+//Create a season method that passes two integers (month,day) as the parameter. 
+//The conditional are based on the seasons of the year and what randon number is entered by the user as a input.
+//The ranges of dates to season are the following  12/16 and 3/15, Winter,3/16 and 6/15 Sping, 9/16 and 12/15 Fall.
+
+import java.util.*;
+
+public class seasonschange{
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the month number: ");
+        int month = input.nextInt();
+        System.out.print("Please enter the day number: ");
+        int day   = input.nextInt();
+        System.out.println(season(month,day));
+    }
+    
+    public static String season(int month, int day){
+        if (month <= 2 || (month == 3 && day <= 15) || (month == 12 && day > 15 )){
+            return "Winter";
+        } else if (month <= 5 || (month == 6 && day <= 15)){
+            return "Spring";
+        } else if (month <= 8 || (month == 9 && day <= 15)){
+            return "Summer";
+        } else {
+            return "Fall";
+        }
+    }
+}
